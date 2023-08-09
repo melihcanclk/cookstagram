fe7787f6b3398d099a246949f902c97ddffc5f08
Melihcan Cilek <melihcanclk@hotmail.com>
2023-08-09 22:28:53 +0300

    update(frontend): home move to profile which user's individual posts

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,39 +6,10 @@ import '../styles/home.css'
 
 export const Home = () => {
 
-    const [posts, setPosts] = useState<IndividualPost[]>([]);
-
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const user = getCookie('user');
-            const userJson = JSON.parse(user);
-            const session = getCookie('session');
-            const res = await fetch(`http://localhost:3000/posts/user/${userJson.username}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${session}`,
-                    'Content-Type': 'application/json'
-                },
-
-            });
-            const data = await res.json();
-            setPosts(data.posts);
-        }
-        fetchPosts();
-    }, []);
 
     return (
         <Layout>
-            <div className='home-container'>
-                <div className="home">
-                    {
-                        posts &&
-                        posts.map((post, key) => (
-                            <IndividualPost post={post} key={key} />
-                        ))
-                    }
-                </div>
-            </div>
+            helllo
         </Layout>
     )
 }
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '../components/layout/Layout'
+import { getCookie } from '../utils/getCookie';
+import { IndividualPost } from '../components/post/IndividualPost';
 
 export const Profile = () => {
+    const [posts, setPosts] = useState<IndividualPost[]>([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const user = getCookie('user');
+            const userJson = JSON.parse(user);
+            const session = getCookie('session');
+            const res = await fetch(`http://localhost:3000/posts/user/${userJson.username}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${session}`,
+                    'Content-Type': 'application/json'
+                },
+
+            });
+            const data = await res.json();
+            setPosts(data.posts);
+        }
+        fetchPosts();
+    }, []);
+
     return (
         <Layout>
-            <h1>Profile</h1>
+            <div className='home-container'>
+                <div className="home">
+                    {
+                        posts &&
+                        posts.map((post, key) => (
+                            <IndividualPost post={post} key={key} />
+                        ))
+                    }
+                </div>
+            </div>
         </Layout>
     )
 }
