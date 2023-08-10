6b4cc845433019a9dcf9e284ea4046508d52ce27
Melihcan Cilek <melihcanclk@hotmail.com>
2023-08-10 03:04:56 +0300

    feat(frontend): wrapper for login modals so they can get center but not layout

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -53,62 +53,63 @@ export const Login = () => {
     }
 
     return (
-        <div className="container">
-
-            <div className="container-msg-modal">
-                <div className="container-modal-content--success container-modal-content">
-                    <span>Welcome!</span>
+        <div className="container-wrapper">
+            <div className="container">
+                <div className="container-msg-modal">
+                    <div className="container-modal-content--success container-modal-content">
+                        <span>Welcome!</span>
+                    </div>
+                    <div className="container-modal-content--error container-modal-content"><span>Failed Login</span></div>
                 </div>
-                <div className="container-modal-content--error container-modal-content"><span>Failed Login</span></div>
-            </div>
 
-            <form onSubmit={
-                handleSubmit(onSubmit)
-            }
-                id='form'
-            >
-
-                <div className="container-form-userName container-form-input">
-                    <label htmlFor="username">Username</label>
-                    <input
-                        id="username"
-                        placeholder="Username"
-                        {...register("username", { required: true })}
-                    />
-                </div>
-                {errors.username && <span className="error-msg">This field is required</span>}
-
-                <div className="container-form-userPassword container-form-input">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Password" onFocus={handleFocus} required
-                        {...register("password", { required: true })}
-                    />
-                </div>
-                <div className='button-container'>
-                    <button type="button" onClick={
-                        () => {
-                            navigate('/register');
-                        }
-                    } className="js-form-btn">Register</button>
-
-                    <button type="submit" className="js-form-btn">Submit</button>
-                </div>
-                {
-                    error && <span className="error-msg">Invalid username or password</span>
+                <form onSubmit={
+                    handleSubmit(onSubmit)
                 }
+                    id='form'
+                >
 
-                <div className="info-msg-container">
+                    <div className="container-form-userName container-form-input">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            id="username"
+                            placeholder="Username"
+                            {...register("username", { required: true })}
+                        />
+                    </div>
+                    {errors.username && <span className="error-msg">This field is required</span>}
+
+                    <div className="container-form-userPassword container-form-input">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Password" onFocus={handleFocus} required
+                            {...register("password", { required: true })}
+                        />
+                    </div>
+                    <div className='button-container'>
+                        <button type="button" onClick={
+                            () => {
+                                navigate('/register');
+                            }
+                        } className="js-form-btn">Register</button>
+
+                        <button type="submit" className="js-form-btn">Submit</button>
+                    </div>
                     {
-                        session && <span className="
+                        error && <span className="error-msg">Invalid username or password</span>
+                    }
+
+                    <div className="info-msg-container">
+                        {
+                            session && <span className="
                         info-msg
                     ">
-                            <a href="/"><LeftArrow height='24px' width='24px' /></a>
-                            You are already logged in</span>
-                    }
-                </div>
-            </form>
+                                <a href="/"><LeftArrow height='24px' width='24px' /></a>
+                                You are already logged in</span>
+                        }
+                    </div>
+                </form>
 
 
+            </div>
         </div>
     );
 }
